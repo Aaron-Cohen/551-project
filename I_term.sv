6c0d68d966090be969b884848027029c78be147e
Aaron-Cohen <aaron.cohen241@gmail.com>
2020-11-18 22:02:44 -0600

    Cleaned up, simplified, and commented I Term code. Added code to PID, but have not made sure that it is guaranteed to fully work in PID.

--- I_term.sv
+++ I_term.sv
@@ -1,3 +1,4 @@
+// Mya Schmitz, Aaron Cohen - 11/18/2020
 module I_term(clk, rst_n, err_sat, err_vld, go, moving, line_present, I_term);
 
 input logic clk, rst_n; 				// clock and reset
@@ -9,55 +10,42 @@ input logic go, moving;				// Integrator is cleared if either is not true
 output logic [9:0] I_term;			// I term for PID
 
 logic ov;							// high if there is overflow, low if not
-logic [15:0] add_vals, full_sum;	// contains value to add?
-logic [15:0] mux1, sum, checkSum;	// output from first multiplexer and adder
-logic [15:0] extended_sat;			// sign extended err_sat value
+logic [15:0] adder_result;	// contains value to add?
+logic [15:0] valid_sum;	// output from first multiplexer and adder
+logic [15:0] err_sat_extended;			// sign extended err_sat value
 logic [15:0] accum_val;				// accumulated value so far
-logic line_reacquired, line_rise, cleared;
+logic line_rise, line_status;
 
-// always_ff @(posedge clk, posedge line_present) begin
-	// if (line_present) // async detector for rising edge of line_present
-		// line_reacquired <= 1;
-	// else // Reset line_reacquired if clock cycle not aligned with rising edge of line_present
-		// line_reacquired <= 0;
-// end
-
-always_ff @(posedge clk) begin
-	if (line_rise || line_present) // async detector for rising edge of line_present
-		cleared <= 1;
-	else // Reset line_reacquired if clock cycle not aligned with rising edge of line_present
-		cleared <= 0;
-end
-
-assign line_rise = cleared ? 0 : line_present;
+// Flopping line_present into sychronous line_status will indicate whether or not there was a line present previously
+always_ff @(posedge clk)
+	line_status <= line_present;
 	
-// sign extend saturated error to 16 bits
-assign extended_sat = {{5{err_sat[10]}}, err_sat};
+// If there is a line present on the syncronous signal, then it has already risen.
+// However, if syncronously there is not a line, but we see one asynchronously, this is a rising edge.
+assign line_rise = line_status ? 0 : line_present;
+	
+// Sign extend saturated error to 16 bits
+assign err_sat_extended = {{5{err_sat[10]}}, err_sat};
 
-// adder block logic
-assign full_sum = accum_val + extended_sat;
-assign sum = (!ov && err_vld) ? full_sum : accum_val;
-assign add_vals = (!go || !moving || line_rise) ? 16'h0000 : sum;
+// Full adder
+assign adder_result = accum_val + err_sat_extended;
 
-// always_ff @(posedge clk, posedge line_present) begin
-	// if (!clk) // async detector for rising edge of line_present
-		// line_reacquired <= 1;
-	// else // Reset line_reacquired if clock cycle not aligned with rising edge of line_present
-		// line_reacquired <= 0;
-// end
+// Mux determining whether full adder has a valid sum based on overflow flag and err_vld to route full adder's result 
+assign valid_sum = (!ov && err_vld) ? adder_result : accum_val;
 
-
-// register for storing accumulated result
+// Register for storing accumulated results
 always_ff @(posedge clk, negedge rst_n) begin
 	if (!rst_n)
 		accum_val <= 16'h0000;
+	else if (!go || !moving || line_rise) // Any of these signals act as sync resets
+		accum_val <= 16'h0000;
 	else
-		accum_val <= add_vals;
+		accum_val <= valid_sum;
 end
 
-// determine if there is overflow
-assign ov = ((accum_val[15] == extended_sat[15]) && (accum_val[15] != full_sum[15]));
+// Overflow occurs when the accumulator and the saturated error have the same sign AND their sum has the opposing sign
+assign ov = ((accum_val[15] == err_sat_extended[15]) && (accum_val[15] != adder_result[15]));
 
-assign I_term = accum_val[15:6]; // get I term for PID
+assign I_term = accum_val[15:6]; // Grab upper bits for I term of PID
 
 endmodule
--- PID.sv
+++ PID.sv
@@ -13,7 +13,7 @@ logic signed [10:0] err_sat;
 assign err_sat = 
 	error[15]  && ~&error[14:10] ? 11'h400 : // If negative and there is a 0 in upper bits, saturate to 100...0
 	!error[15] && |error[14:10]  ? 11'h3FF : // If positive and there is a 1 in upper bits, saturate to 011...1
-					{error[15], error[9:0]}; // else, do not saturate: tack sign bit to lower 10 bits.		
+			{error[15], error[9:0]}; // else, do not saturate: tack sign bit to lower 10 bits.		
 
 ////////////////////////////////////////////
 //  Signals and calculations for P_term  //
@@ -27,26 +27,48 @@ assign err_product = err_sat * P_COEFF;
 assign P_term = 
 	err_product[16] && ~&err_product[15:14] ? 15'h4000 	: // If negative and there is a 0 in upper bits, saturate to 100...0
 	!err_product[16] && |err_product[15:14] ? 15'h3FFF 	: // If positive and there is a 1 in upper bits, saturate to 011...1
-					{err_product[16], err_product[13:0]}; // else, do not saturate: tack sign bit to lower 14 bits.	
+			{err_product[16], err_product[13:0]}; // else, do not saturate: tack sign bit to lower 14 bits.	
 
 
 
 ////////////////////////////////////////////////
 //  Signals, calculations, flops for I_term  //
 //////////////////////////////////////////////
+logic [9:0] I_term
+logic ov;							// Indicates whether overflow has occured
+logic [15:0] adder_result;			// Full adder sum
+logic [15:0] valid_sum;				// Sum post validation (retains value if invalid sum)
+logic [15:0] err_sat_extended;		// Sign extended err_sat value
+logic [15:0] accum_val;				// Accumulated value so far
+logic line_rise, line_status;		// Status of mazerunner on line
 
+// If there is a line present on the syncronous signal, then it has already risen.
+// However, if syncronously there is not a line, but we see one asynchronously, this is a rising edge.
+assign line_rise = line_status ? 0 : line_present;
+	
+// Sign extend saturated error to 16 bits
+assign err_sat_extended = {{5{err_sat[10]}}, err_sat};
 
+// Full adder
+assign adder_result = accum_val + err_sat_extended;
 
+// Mux determining whether full adder has a valid sum based on overflow flag and err_vld to route full adder's result 
+assign valid_sum = (!ov && err_vld) ? adder_result : accum_val;
 
+// Register for storing accumulated results
+always_ff @(posedge clk, negedge rst_n) begin
+	if (!rst_n)
+		accum_val <= 16'h0000;
+	else if (!go || !moving || line_rise) // Any of these signals act as sync resets
+		accum_val <= 16'h0000;
+	else
+		accum_val <= valid_sum;
+end
 
+// Overflow occurs when the accumulator and the saturated error have the same sign AND their sum has the opposing sign
+assign ov = ((accum_val[15] == err_sat_extended[15]) && (accum_val[15] != adder_result[15]));
 
-
-
-
-
-
-
-
+assign I_term = accum_val[15:6]; // Grab upper bits for I term of PID
 
 ////////////////////////////////////////////////
 //  Signals, calculations, flops for D_term  //
@@ -75,7 +97,7 @@ assign D_diff = err_sat - err_sat_2x_old;
 assign D_diff_sat = 
 	D_diff[10] && ~&D_diff[10:7] ? 8'h80 : // If negative and there is a 0 in upper bits, saturate to 100...0
 	!D_diff[10] && |D_diff[10:7] ? 8'h7F : // If positive and there is a 1 in upper bits, saturate to 011...1
-		{D_diff[10], D_diff[6:0]}; // else, do not saturate: tack sign bit to lower 7 bits.	
+			{D_diff[10], D_diff[6:0]}; // else, do not saturate: tack sign bit to lower 7 bits.	
 
 assign D_term = D_COEFF * D_diff_sat;
 
