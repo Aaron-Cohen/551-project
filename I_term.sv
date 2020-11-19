54cb375452f34f118048bf66168336937ebc5d12
myaschmitz <mschmitz9@wisc.edu>
2020-11-18 18:13:44 -0600

    I_term passing to test 9

--- I_term.sv
+++ I_term.sv
@@ -1,60 +1,54 @@
 module I_term(clk, rst_n, err_sat, err_vld, go, moving, line_present, I_term);
 
-input clk, rst_n; 			// clock and reset
-input line_present;			// Integrator is cleared on rise of line_present
-input signed [10:0] err_sat;		// saturated value of error (signed)
+input clk, rst_n; 				// clock and reset
+input line_present;				// Integrator is cleared on rise of line_present
+input [10:0] err_sat;			// saturated value of error (signed)
+input err_vld;					// high when err_sat is valid
+input go, moving;				// Integrator is cleared if either is not true
 
-input err_vld;				// high when err_sat is valid
-input go, moving;			// Integrator is cleared if either is not true
-output signed [9:0] I_term;		// I term for PID
+output [9:0] I_term;			// I term for PID
 
-logic ov;					// high if there is overflow, low if not
-logic signed [15:0] mux1, sum;		// output from first multiplexer, adder
-logic signed [15:0] extended_sat;	// sign extended err_sat value
-logic signed [15:0] accum_val;		// accumulated value so far
+logic ov;							// high if there is overflow, low if not
+logic [15:0] add_vals, full_sum;	// contains value to add?
+logic [15:0] mux1, sum, checkSum;	// output from first multiplexer and adder
+logic [15:0] extended_sat;			// sign extended err_sat value
+logic [15:0] accum_val;				// accumulated value so far
+logic line_reacquired;
 
+always_ff @(posedge clk) begin
+	if (line_present) // async detector for rising edge of line_present
+		line_reacquired <= 1;
+	else // Reset line_reacquired if clock cycle not aligned with rising edge of line_present
+		line_reacquired <= 0;
+end
 	
 // sign extend saturated error to 16 bits
-assign extended_sat = {{15{err_sat[10]}}, err_sat[9:0]};
+assign extended_sat = {{5{err_sat[10]}}, err_sat};
 
-// accumulate and determine if there's overflow
-always_comb begin
-	ov = 0; // Overflow can only occur if same signs on addition operands
-	if (accum_val[15] == extended_sat[15]) begin
-		if(accum_val[15] != sum[15])
-			ov = 1;
-	end
-end
+// adder block logic
+assign full_sum = accum_val + extended_sat;
+assign sum = (!ov && err_vld) ? full_sum : accum_val;
+assign add_vals = (!go || !moving || !line_reacquired) ? 16'h0000 : sum;
 
-// adder block
-assign sum = accum_val + extended_sat;
+// always_ff @(posedge clk, posedge line_present) begin
+	// if (!clk) // async detector for rising edge of line_present
+		// line_reacquired <= 1;
+	// else // Reset line_reacquired if clock cycle not aligned with rising edge of line_present
+		// line_reacquired <= 0;
+// end
 
-assign mux1 = (err_vld && !ov) ? sum : I_term;
-
-logic line_reaquired;
-always @(posedge clk, posedge line_present) begin
-	if(!clk) // async detector for rising edge of line_present
-		line_reaquired <= 1;
-	else // Reset line_reaquired if clock cycle not aligned with rising edge of line_present
-		line_reaquired <= 0;
-end
 
 // register for storing accumulated result
 always_ff @(posedge clk, negedge rst_n) begin
 	if (!rst_n)
 		accum_val <= 16'h0000;
-	else if (line_reaquired || !moving || !go)
-		accum_val <= 16'h0000;
 	else
-		accum_val <= mux1;
+		accum_val <= add_vals;
 end
-assign I_term = accum_val[15:6];
 
-// always (@posedge line_present) begin
+// determine if there is overflow
+assign ov = ((accum_val[15] == extended_sat[15]) && (accum_val[15] != full_sum[15]));
 
-	// assign I_term = (!moving || !go || line_present) ? 16'h0000 : 
-	
-	
-// end
+assign I_term = accum_val[15:6]; // get I term for PID
 
 endmodule
