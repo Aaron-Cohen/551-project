372191cbacd2bac978af5d1ba4613ac9c4a2e118
DustinEsser <60329741+DustinEsser@users.noreply.github.com>
2020-12-02 10:51:00 -0600

    This is just the two tests that Hoffman gave us

--- MazeRunner_tb.sv
+++ MazeRunner_tb.sv
@@ -17,6 +17,53 @@ module MazeRunner_tb();
 	wire cmd_sent;					// probably don't need this
 	wire buzz,buzz_n;				// hooked to piezo buzzer outputs
 	
+	///////////////////////////////////////////////////////////////
+	// Declare Testing variables							    //
+	/////////////////////////////////////////////////////////////
+	parameter TEST = 1;
+	reg signed [12:0] theta1, theta2, theta3, theta4;
+	int line_gone_clks;
+	
+	generate
+	
+	  if(TEST == 1) begin
+		
+		/**
+		*  TEST 1 command is veer right
+		*
+		*/
+		assign cmd = 16'h5555;
+		assign theta1 = 150;
+		assign theta2 = 500;
+		
+		assign line_gone_clks = 300000;
+		
+		
+	  
+	  end else if(TEST == 2) begin
+	    
+		/**
+		*  TEST 2 command is turn around
+		*
+		*/
+		assign cmd = 16'hFFFF;
+		assign theta1 = 150;
+		assign theta2 = -1650;
+		
+		assign line_gone_clks = 1750000;
+	  
+	  
+	  
+	  end
+	
+	
+	
+	
+	
+	endgenerate
+	
+	
+	
     //////////////////////
 	// Instantiate DUT //
 	////////////////////
@@ -40,10 +87,64 @@ module MazeRunner_tb();
 		
 
 	initial begin
-      <  you fill in the details of what you are testing >
+      clk = 0;
+	  RST_n = 0;
+	  send_cmd = 0;
+	  line_theta = 0; 
+	  line_present = 1;
+	  BMPL_n = 1;
+	  BMPR_n = 1;
+	  
+	  wait_clk_cycl(5, clk);
+	  
+	  send_cmd = 1;
+	  RST_n = 1;
+	  
+	  //wait to get up to speed
+	  wait_clk_cycl(1500000, clk);
+	  
+	  //change line theta
+	  line_theta = theta1;
+	  
+	  //wait for the manuver 
+	  wait_clk_cycl(1000000, clk);
+	  
+	  //line gone
+	  remove_line(line_gone_clks, clk, line_present);
+	  
+	  //new theta
+	  line_theta = theta2;
+	  
+	  
+	  
 	end
 	
 	always
 	  #5 clk = ~clk;
 				  
 endmodule
+
+
+//task that waits a given amount of clock cycles
+task wait_clk_cycl;
+  input int clk_cycl;
+  input clk;
+  
+  repeat(clk_cycl)@(negedge clk);
+  
+endtask
+
+//task that removes the line for a given amount of clk cycles
+task remove_line;
+  input int cycl_gone;
+  input clk;
+  input line_present;
+  
+  line_present = 0;
+  
+  wait_clk_cycl(cycl_gone, clk);
+  
+  line_present = 1;
+  
+endtask
+	
