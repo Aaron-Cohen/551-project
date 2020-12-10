1e4d6825d8e116db2cf4b73d5b77ce8af30c62f3
Aaron-Cohen <aaron.cohen241@gmail.com>
2020-12-10 12:45:13 -0600

    Add pipeline flop to mtr_drv

--- mtr_drv.sv
+++ mtr_drv.sv
@@ -5,9 +5,16 @@ module mtr_drv(clk, rst_n, lft_duty, rght_duty, DIRL, DIRR, PWML, PWMR);
 	output PWML, PWMR;
 	logic [10:0] lft_duty_mag, rght_duty_mag;
 	
+	// Pipeline inputs left duty, right duty
+	logic [11:0] lft_duty_ff, rght_duty_ff;
+	always @(posedge clk) begin
+		lft_duty_ff <= lft_duty;
+		rght_duty_ff <= rght_duty;
+	end
+	
 	// Get absolute value before feeding magnitude to PWM
-	assign lft_duty_mag  = lft_duty[11]  ? ~lft_duty[10:0]  + 1 : lft_duty[10:0];
-	assign rght_duty_mag = rght_duty[11] ? ~rght_duty[10:0] + 1 : rght_duty[10:0];
+	assign lft_duty_mag  = lft_duty_ff[11]  ? ~lft_duty_ff[10:0]  + 1 : lft_duty_ff[10:0];
+	assign rght_duty_mag = rght_duty_ff[11] ? ~rght_duty_ff[10:0] + 1 : rght_duty_ff[10:0];
 	
 	// Instantiate PWM's with magnitudes determined above
 	PWM11 lftPWM( .clk(clk), .rst_n(rst_n), .duty(lft_duty_mag),  .PWM_sig(PWML));
